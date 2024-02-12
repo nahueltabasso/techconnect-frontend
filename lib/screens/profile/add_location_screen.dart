4332ab2b2d7fb98519ffa52b19f27274996c4e9b
nahueltabasso <nahueltabasso@gmail.com>
2024-02-11 23:33:29 -0300

    fix: add a marker on google map

--- lib/screens/profile/add_location_screen.dart
+++ lib/screens/profile/add_location_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 
 class AddLocationScreen extends StatefulWidget {
   const AddLocationScreen({super.key});
@@ -13,10 +15,14 @@ class AddLocationScreen extends StatefulWidget {
 class _AddLocationScreenState extends State<AddLocationScreen> {
   
   static const LatLng _center = const LatLng(37.4234, -122.6848);
-  MapType _currentMapType = MapType.normal;
-  final Set<Marker> _markers = {};
   GoogleMapController? _controller;
-
+  MapType _currentMapType = MapType.normal;
+  Set<Marker> _markers = {};
+  CameraPosition initialCameraPosition = const CameraPosition(
+    zoom: 11,
+    target: LatLng(37.4234, -122.6848),
+  );
+  BitmapDescriptor icon = BitmapDescriptor.defaultMarker;
 
   void _onMapCreated(GoogleMapController controller) {
     _controller = controller;
@@ -34,6 +40,24 @@ class _AddLocationScreenState extends State<AddLocationScreen> {
     });
   }
 
+  void _addLocation(LatLng position) {
+    print(position);
+    _markers.add(
+      Marker(
+        markerId: MarkerId(position.toString()),
+        position: position,
+        icon: icon,
+        infoWindow: InfoWindow(title: DateFormat('dd/MM/yyyy hh:mm a').format(DateTime.now())),
+      )
+    );
+    moveCamera(position);
+    setState(() {});
+  }
+
+  Future<void> moveCamera(LatLng position) async {
+    // final controller = await googleMapController.future;
+    _controller?.animateCamera(CameraUpdate.newLatLng(position));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,30 +66,37 @@ class _AddLocationScreenState extends State<AddLocationScreen> {
         title: const Center(child: Text('Selecciona tu ubicacion')),
         backgroundColor: Colors.lightBlue,
       ),
-      body: Stack(
-        children: <Widget>[
-          GoogleMap(
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: const CameraPosition(target: _center, zoom: 10),
-            mapType: _currentMapType,
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: FloatingActionButton(
-                materialTapTargetSize: MaterialTapTargetSize.padded,
-                backgroundColor: Colors.lightBlue,
-                child: const Icon(Icons.map, size: 30.0),
-                onPressed: () => _onMapTypeChange(),
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: initialCameraPosition,
+              mapType: _currentMapType,
+              markers: _markers,
+              onTap: (LatLng position) {
+                _addLocation(position);
+              },
+            ),
+        
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: FloatingActionButton(
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.lightBlue,
+                  child: const Icon(Icons.map, size: 30.0),
+                  onPressed: () => _onMapTypeChange(),
+                ),
               ),
             ),
-          ),
-
-          const SizedBox(height: 10,),
-        ]
+        
+            const SizedBox(height: 10,),
+          ]
+        ),
       )
     );
   }
 }
+
